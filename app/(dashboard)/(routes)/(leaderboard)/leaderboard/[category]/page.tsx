8739dd9edb9894a2918f900296a109118fe00a5a
Habib7442 <habibtanwir1906@gmail.com>
2024-07-07 08:36:14 +0530

    rectified image problem in leaderboard

--- app/(dashboard)/(routes)/(leaderboard)/leaderboard/[category]/page.tsx
+++ app/(dashboard)/(routes)/(leaderboard)/leaderboard/[category]/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import { fetchScores } from "@/utils/firebase";
-import { DocumentData } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Loader2Icon } from "lucide-react";
 
 type LeaderboardItem = {
   id: number;
@@ -28,6 +20,9 @@ const Leaderboard = ({ params }: any) => {
   const { category } = params;
   const [data, setData] = useState<LeaderboardItem[]>([]);
   const [roomId, setRoomId] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
 
   useEffect(() => {
     fetchLeaderboard();
@@ -35,12 +30,9 @@ const Leaderboard = ({ params }: any) => {
 
   const fetchLeaderboard = async () => {
     try {
-      const response = await fetchScores(
-        category === "roomId" ? roomId : category
-      );
-      const sortedData = (response ?? []).sort(
-        (a: any, b: any) => b.score - a.score
-      );
+      setLoading(true)
+      const response = await fetchScores(category === "roomId" ? roomId : category);
+      const sortedData = (response ?? []).sort((a: any, b: any) => b.score - a.score);
       const transformedData = sortedData.map((item: any, index: number) => ({
         id: index,
         name: item.name,
@@ -51,41 +43,19 @@ const Leaderboard = ({ params }: any) => {
       setData(transformedData);
     } catch (error) {
       console.log(error);
+    } finally{
+      setLoading(false)
     }
   };
 
   const getPositionBadge = (index: number) => {
     switch (index) {
       case 0:
-        return (
-          <Image
-            width={100}
-            height={100}
-            src="/first.png"
-            alt="1st place"
-            className="h-10 w-10 mx-auto rounded-full"
-          />
-        );
+        return <Image width={100} height={100} src="/first.png" alt="1st place" className="h-10 w-10 mx-auto rounded-full" />;
       case 1:
-        return (
-          <Image
-            width={100}
-            height={100}
-            src="/second.jfif"
-            alt="2nd place"
-            className="h-10 w-10 mx-auto rounded-full"
-          />
-        );
+        return <Image width={100} height={100} src="/second.jfif" alt="2nd place" className="h-10 w-10 mx-auto rounded-full" />;
       case 2:
-        return (
-          <Image
-            width={100}
-            height={100}
-            src="/third.jfif"
-            alt="3rd place"
-            className="h-10 w-10 mx-auto rounded-full"
-          />
-        );
+        return <Image width={100} height={100} src="/third.jfif" alt="3rd place" className="h-10 w-10 mx-auto rounded-full" />;
       default:
         return <span className="text-center">{index + 1}</span>;
     }
@@ -106,38 +76,40 @@ const Leaderboard = ({ params }: any) => {
         </div>
       )}
       <div className="w-full lg:w-2/3">
-        <Table>
-          <TableCaption>Leaderboard score will be shown here</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-1/12 text-center">Position</TableHead>
-              <TableHead className="w-1/4 text-center">Avatar</TableHead>
-              <TableHead className="w-1/4 text-center">Name</TableHead>
-              <TableHead className="w-1/4 text-center">Score</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell className="text-center">
-                  {getPositionBadge(index)}
-                </TableCell>
-                <TableCell className="w-full flex justify-center">
-                  <AnimatedTooltip items={data} />
-                </TableCell>
-                <TableCell className="font-medium text-center">
-                  {item.name}
-                </TableCell>
-                <TableCell className="font-medium text-center">
-                  {item.score}
-                </TableCell>
+        {loading ? (
+          <div className="flex justify-center items-center min-h-screen">
+            <Loader2Icon className="w-12 h-12 animate-spin" />
+          </div>
+        ) : (
+          <Table>
+            <TableCaption>Leaderboard score will be shown here</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/12 text-center">Position</TableHead>
+                <TableHead className="w-1/4 text-center">Avatar</TableHead>
+                <TableHead className="w-1/4 text-center">Name</TableHead>
+                <TableHead className="w-1/4 text-center">Score</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {data.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className="text-center">{getPositionBadge(index)}</TableCell>
+                  <TableCell className="w-full flex justify-center">
+                    <AnimatedTooltip item={item} />
+                  </TableCell>
+                  <TableCell className="font-medium text-center">{item.name}</TableCell>
+                  <TableCell className="font-medium text-center">{item.score}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </div>
     </div>
   );
+  
 };
 
 export default Leaderboard;
+``
